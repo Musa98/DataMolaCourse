5513c909ae0484919e8e4fd8e2e2f2406d696ccb
Musa98 <gsunnymus@gmail.com>
2022-04-22 23:05:52 +0300

    feat: add getDate,postregistration and postLofin

--- TwitTask/UI/script.js
+++ TwitTask/UI/script.js
@@ -1,11 +1,12 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
 class TweetsController {
   constructor() {
-    this.tweetCollection = new TweetCollection(tweets);
+    // this.tweetCollection = new TweetCollection(tweets);
     this.userList = new UserList();
     this.header = new HeaderView('user');
     this.tweetFeed = new TweetFeedView('tweet-collection');
@@ -35,11 +36,12 @@ class TweetsController {
     this.tweetFeed.display(this.tweetCollection);
   }
 
-  getFeed(skip = 0, top = 10, obj = null) {
+  getFeed(result, skip = 0, top = 10, obj = null) {
     if (obj === null) {
-      this.tweetFeed.display(this.tweetCollection, skip, top);
+      // this.tweetFeed.display(this.tweetCollection, skip, top);
+      this.tweetFeed.display(result, skip, top);
     } else {
-      this.filter.display(this.tweetCollection, obj, skip, top);
+      this.filter.display(result, obj, skip, top);
     }
   }
 
@@ -53,30 +55,133 @@ class TweetsController {
   }
 }
 
-if (!(localStorage.getItem('tweetArr') && localStorage.getItem('userArr'))) {
-  localStorage.setItem('currentUser', JSON.stringify(new TweetCollection(tweets).user));
-  localStorage.setItem('tweetArr', JSON.stringify(new TweetCollection(tweets).tweetsArr));
-  localStorage.setItem('userArr', JSON.stringify(new UserList(listUser).arrayUser));
+class TweetFeedApiService {
+  constructor(ip) {
+    this.ip = ip;
+  }
+
+  async getData(from = 0, count = 10, author = '', dateFrom = '', dateTo = '', text = '', hashtags = '') {
+    try {
+      if (author) {
+        author = `author=${author}&`;
+      }
+
+      if (text) {
+        text = `text=${text}&`;
+      }
+
+      if (dateFrom) {
+        dateFrom = `dateFrom=${dateFrom}T00%3A00%3A00.001Z&`;
+      }
+
+      if (dateTo) {
+        dateTo = `dateTo=${dateTo}T00%3A00%3A00.001Z&`;
+      }
+
+      if (hashtags) {
+        let resultTags = '';
+        hashtags.split(' ').forEach((element, index, arr) => {
+          if (index === arr.length - 1) {
+            resultTags += `${element.substring(1)}`;
+          } else {
+            resultTags += `${element.substring(1)}%2C`;
+          }
+        });
+        hashtags = `&hashtags=${resultTags}`;
+      }
+      // authorFil = `author=${authorFil}` || '';
+      const response = await fetch(`https://jslabapi.datamola.com/tweet?${author}${text}${dateFrom}${dateTo}from=${from}&count=${count}${hashtags}`);
+      const result = await response.json();
+      console.log(result);
+      console.log(response.status);
+      const tweetCollection = new TweetCollection(result);
+      // tweetController.tweetCollection.tweetsArr = result;
+      // tweetController.getFeed();
+      return tweetCollection;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async postLogin(data) {
+    const response = await fetch('https://jslabapi.datamola.com/login', {
+      method: 'POST',
+      headers: {
+        ['Content-Type']: 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+
+  async postRegistration(data) {
+    const response = await fetch('https://jslabapi.datamola.com/registration', {
+      method: 'POST',
+      headers: {
+        ['Content-Type']: 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+
+  async postTweet(test) {
+    const response = await fetch('https://jslabapi.datamola.com/tweet', {
+      method: 'POST',
+      headers: {
+        ['Content-Type']: 'application/json',
+        authorization: `Bearer ${test.token}`
+      },
+      body: JSON.stringify(test)
+    });
+    return response.json();
+  }
 }
 
+// if (!(localStorage.getItem('tweetArr') && localStorage.getItem('userArr'))) {
+//   localStorage.setItem('currentUser', JSON.stringify(new TweetCollection(tweets).user));
+//   localStorage.setItem('tweetArr', JSON.stringify(new TweetCollection(tweets).tweetsArr));
+//   localStorage.setItem('userArr', JSON.stringify(new UserList(listUser).arrayUser));
+// }
+
+const tweetApi = new TweetFeedApiService();
 const tweetController = new TweetsController();
 
-if (localStorage.getItem('currentUser')) {
-  tweetController.tweetCollection.user = tweetController.tweetCollection.restoreUser();
-  tweetController.setCurrentUser(tweetController.tweetCollection.user);
-} else {
-  tweetController.tweetCollection.user = null;
-}
+// if (localStorage.getItem('user')) {
+//   tweetController.tweetCollection.user = tweetController.tweetCollection.restoreUser();
+//   tweetController.setCurrentUser(tweetController.tweetCollection.user);
+// } else {
+//   tweetController.tweetCollection.user = null;
+// }
 
 function loadTweetApp(event) {
   const tagArray = [];
   const loadTweets = (e) => {
     if (e.target.classList.contains('load-button')) {
       loadTweets.counter++;
-      tweetController.getFeed(0, loadTweets.counter * 10);
-      if (loadTweets.counter * 10 < tweetController.tweetCollection.tweetsArr.length) { //!
-        document.querySelector('.load-button').classList.add('active-load-button');
+      if (document['filter'][0] || document['filter'][1] || document['filter'][2] || document['filter'][3] || document['filter'][4]) {
+        tweetApi.getData(0, loadTweets.counter * 10, document['filter'][0].value).then(
+          result => {
+            submitFilter();
+            if (loadTweets.counter * 10 <= result.tweetsArr.length) {
+              document.querySelector('.load-button').classList.add('active-load-button');
+            }
+          }
+        );//
+      } else {
+        tweetApi.getData(0, loadTweets.counter * 10).then(
+          result => {
+            tweetController.getFeed(result, 0, loadTweets.counter * 10);
+            if (loadTweets.counter * 10 <= result.tweetsArr.length) {
+              document.querySelector('.load-button').classList.add('active-load-button');
+            }
+          }
+        );//!
       }
+      // tweetController.getFeed(0, loadTweets.counter * 10);
+      // if (loadTweets.counter * 10 < tweetController.tweetCollection.tweetsArr.length) { //!
+      //   document.querySelector('.load-button').classList.add('active-load-button');
+      // }
     }
   };
   const addHandler = (e) => {
@@ -126,7 +231,7 @@ function loadTweetApp(event) {
     }
   };
   const submitFilter = (e) => {
-    e.preventDefault();
+    e?.preventDefault();
     const filterBlock = document.filter;
     const filterObj = {};
     if (filterBlock[0].value) {
@@ -144,7 +249,18 @@ function loadTweetApp(event) {
     if (filterBlock[3].value) {
       filterObj.text = filterBlock[3].value;
     }
-    tweetController.getFeed(0, 10, filterObj);
+    if (e) {
+      loadTweets.counter = 1;
+    }
+    tweetApi.getData(0, loadTweets.counter * 10, filterBlock[0].value, filterBlock[1].value, filterBlock[2].value, filterBlock[3].value, filterObj.hashtags).then(
+      result => {
+        tweetController.getFeed(result, 0, 10, filterObj);
+        if (loadTweets.counter * 10 <= result.tweetsArr.length) {
+          document.querySelector('.load-button').classList.add('active-load-button');
+        }
+      }
+    );//!
+    // tweetController.getFeed(0, 10, filterObj);
     filterBlock[4].value = '';
   };
   const showTweetPage = (e) => {
@@ -161,10 +277,18 @@ function loadTweetApp(event) {
 
   const returnMainTweet = (e) => {
     if (e.target.classList.contains('main-button') || e.target.parentElement.classList.contains('main-button')) {
-      tweetController.getFeed();
-      if (loadTweets.counter * 10 < tweetController.tweetCollection.tweetsArr.length) { //!
-        document.querySelector('.load-button').classList.add('active-load-button');
-      }
+      tweetApi.getData(0, loadTweets.counter * 10).then(
+        result => {
+          tweetController.getFeed(result);
+          if (loadTweets.counter * 10 <= result.tweetsArr.length) {
+            document.querySelector('.load-button').classList.add('active-load-button');
+          }
+        }
+      );//!
+      // tweetController.getFeed();
+      // if (loadTweets.counter * 10 < tweetController.tweetCollection.tweetsArr.length) { //!
+      //   document.querySelector('.load-button').classList.add('active-load-button');
+      // }
     }
   };
   const loginPage = (e) => {
@@ -183,39 +307,87 @@ function loadTweetApp(event) {
     }
   };
   const registerNewUser = (e) => {
-    let newUser;
+    // let newUser;
     e.preventDefault();
     if (e.target === document.querySelector('.registration-button')) {
-      newUser = tweetController.userList
-        .registerUser(register[0].value, register[1].value, register[2].value);
-      if (newUser) {
-        tweetController.loginPage.display();
+      if (register[1].value !== register[2].value) {
+        alert('Password and repeatpassword don\'t same');
+        return null;
       }
+      tweetApi.postRegistration({
+        ['login']: register[0].value,
+        ['password']: register[1].value
+      }).then(
+        result => {
+          if (result.statusCode === 409) {
+            alert('Login is alredy exists');
+          } else {
+            tweetController.loginPage.display();
+          }
+        }
+      );
+      // newUser = tweetController.userList
+      //   .registerUser(register[0].value, register[1].value, register[2].value);
+      // if (newUser) {
+      // }
     }
+    return null;
   };
   const authorizeCurrentUser = (e) => {
-    let user;
+    // let user;
     e.preventDefault();
-    if (e.target === document.querySelector('.enter-buttons')?.lastElementChild) {
-      user = tweetController.userList
-        .authorizeUser(authorization[0].value, authorization[1].value);
-      if (user) {
-        tweetController.setCurrentUser(user);
-        tweetController.getFeed();
-      } else {
-        alert('Not found User');
-        authorization[0].value = '';
-        authorization[1].value = '';
-      }
+    if (e.target === document.querySelector('.authorize')) {
+      // user = tweetController.userList
+      //   .authorizeUser(authorization[0].value, authorization[1].value);
+      tweetApi.postLogin({
+        ['login']: authorization[0].value,
+        ['password']: authorization[1].value
+      }).then(result =>{
+        if (result.token) {
+          localStorage.setItem('user', authorization[0].value);
+          tweetApi.getData(0, loadTweets.counter * 10).then(
+            resultGetFeed => {
+              resultGetFeed.user = localStorage.getItem('user');
+              tweetController.getFeed(resultGetFeed);
+              if (loadTweets.counter * 10 <= resultGetFeed.tweetsArr.length) {
+                document.querySelector('.load-button').classList.add('active-load-button');
+              }
+            }
+          );
+        }
+        if (result.statusCode === 403) {
+          alert('Incorrect login or password');
+        }
+        // console.log(result);
+      });
+      // if (user) {
+      //   tweetController.setCurrentUser(user);
+      //   tweetController.getFeed();
+      // } else {
+      //   alert('Not found User');
+      //   authorization[0].value = '';
+      //   authorization[1].value = '';
+      // }
     }
   };
   const exitProfile = (e) => {
     e.preventDefault();
     if (e.target === document.querySelector('.exit-button')) {
-      tweetController.tweetCollection.user = null;
-      tweetController.userList.saveUser(null);
-      tweetController.getFeed();
-      document.querySelector('.container-menu-user').classList.remove('twit-container-menu-user');
+      localStorage.removeItem('user');
+      tweetApi.getData(0, loadTweets.counter * 10).then(
+        resultGetFeed => {
+          resultGetFeed.user = null;
+          tweetController.getFeed(resultGetFeed);
+          document.querySelector('.container-menu-user').classList.remove('twit-container-menu-user');
+          if (loadTweets.counter * 10 <= resultGetFeed.tweetsArr.length) {
+            document.querySelector('.load-button').classList.add('active-load-button');
+          }
+        }
+      );
+      // tweetController.tweetCollection.user = null;
+      // tweetController.userList.saveUser(null);
+      // tweetController.getFeed();
+      // document.querySelector('.container-menu-user').classList.remove('twit-container-menu-user');
     }
   };
   const checkLengthText = (e) => {
@@ -271,6 +443,7 @@ function loadTweetApp(event) {
   saveFilterButton.addEventListener('click', submitFilter);
   // load button
   loadTweets.counter = 1;
+  // mainTweet.addEventListener('click', loadTweets);
   mainTweet.addEventListener('click', loadTweets);
   // show tweet
   mainTweet.addEventListener('click', showTweetPage);
@@ -296,27 +469,47 @@ function loadTweetApp(event) {
   buttonAddTag.addEventListener('click', addHashTag);
   const filter = document.querySelector('.container-filter');
   filter.addEventListener('click', deleteHashTag);
-  tweetController.getFeed();
-  if (loadTweets.counter * 10 < tweetController.tweetCollection.tweetsArr.length) {
-    document.querySelector('.load-button').classList.add('active-load-button');
-  }
-  if (tweetController.tweetCollection.user === null) {
-    if (document.querySelector('.container-add-twit')) {
-      document.querySelector('.container-add-twit').remove();
-    }
-    document.querySelector('aside').classList.add('without-login');
-    document.querySelector('.container-menu-user').innerHTML = '<button class="login-button">Login</button>';
-    document.querySelector('.container-menu-user').classList.add('header-login-button');
+  if (localStorage.getItem('user')) {
+    tweetApi.getData(0, loadTweets.counter * 10).then(
+      resultGetFeed => {
+        resultGetFeed.user = localStorage.getItem('user');
+        tweetController.getFeed(resultGetFeed);
+        if (loadTweets.counter * 10 <= resultGetFeed.tweetsArr.length) {
+          document.querySelector('.load-button').classList.add('active-load-button');
+        }
+      }
+    );
   } else {
-    document.querySelector('aside').classList.remove('without-login');
-    document.querySelector('.container-menu-user').innerHTML = `
-      <div class="container-menu-user">
-        <span id="user" class="text-user-name">Musa</span>
-        <button class="exit-button">Exit</button>
-      </div>
-    `;
-    document.querySelector('.container-menu-user').classList.remove('header-login-button');
+    tweetApi.getData(0, loadTweets.counter * 10).then(
+      result => {
+        tweetController.getFeed(result);
+        if (loadTweets.counter * 10 <= result.tweetsArr.length) {
+          document.querySelector('.load-button').classList.add('active-load-button');
+        }
+      }
+    );//!
   }
+  // tweetController.getFeed();
+  // if (loadTweets.counter * 10 < tweetController.tweetCollection.tweetsArr.length) {
+  //   document.querySelector('.load-button').classList.add('active-load-button');
+  // }
+  // if (tweetController.tweetCollection.user === null) {
+  //   if (document.querySelector('.container-add-twit')) {
+  //     document.querySelector('.container-add-twit').remove();
+  //   }
+  //   document.querySelector('aside').classList.add('without-login');
+  //   document.querySelector('.container-menu-user').innerHTML = '<button class="login-button">Login</button>';
+  //   document.querySelector('.container-menu-user').classList.add('header-login-button');
+  // } else {
+  //   document.querySelector('aside').classList.remove('without-login');
+  //   document.querySelector('.container-menu-user').innerHTML = `
+  //     <div class="container-menu-user">
+  //       <span id="user" class="text-user-name">Musa</span>
+  //       <button class="exit-button">Exit</button>
+  //     </div>
+  //   `;
+  //   document.querySelector('.container-menu-user').classList.remove('header-login-button');
+  // }
 }
 
 window.addEventListener('load', loadTweetApp);
